a1ec2f76869af1fbd8b2704336236bc6bf4b61ff
wandersoncbjr <wandersoncbjr2@gmail.com>
2022-11-29 15:24:37 -0300

    fetch na apod API

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ btn.addEventListener("click", botao);
 function botao() {
 
 }
+
+async function PegandoInformacao() {
+    var input = document.querySelector("#Data").value;
+    const apikey = "OAOAqeOd3ANt51hiWbk2eskSCl6X7B7ZKZaZfzAQ";
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${apikey}&date=${input}`;
+    console.log(input);
+    const resposta = await fetch(url);
+    const data = await resposta.json();
+    console.log(data);
+    console.log(data.explanation);
+}
