8e017f7c22cffaf5b858eba1d0ccc35a495323be
wandersoncbjr <wandersoncbjr2@gmail.com>
2022-11-30 15:14:01 -0300

    feat: consumindo api, img e text

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const btn = document.querySelector("#botao");
 btn.addEventListener("click", botao);
 function botao() {
-    PegandoInformacao()
+  PegandoInformacao();
 }
 
 async function PegandoInformacao() {
-    var input = document.querySelector("#Data").value;
-    const apikey = "OAOAqeOd3ANt51hiWbk2eskSCl6X7B7ZKZaZfzAQ";
-    const url = `https://api.nasa.gov/planetary/apod?api_key=${apikey}&date=${input}`;
-    console.log(input);
-    const resposta = await fetch(url);
-    const data = await resposta.json();
-    console.log(data);
-    console.log(data.explanation);
-    document.getElementById("oi").innerHTML = data.explanation;
-    document.getElementById("data").innerHTML = data.date;
-    document.getElementsByTagName("body")[0].style = `background-image:url(${data.url});`;
+  let input = document.querySelector("#inputData").value;
+  const apikey = "OAOAqeOd3ANt51hiWbk2eskSCl6X7B7ZKZaZfzAQ";
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${apikey}&date=${input}`;
+  const resposta = await fetch(url);
+  const data = await resposta.json();
+  document.getElementById("data").innerHTML = data.date;
+  document.querySelector("#legenda").innerText = data.explanation;
+  document.getElementsByTagName(
+    "body"
+  )[0].style = `background-image:url(${data.url});`;
 }
