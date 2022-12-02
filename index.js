8d64103cfeaff2ab291c4b6fefc6c20761ea722d
wandersoncbjr <wandersoncbjr2@gmail.com>
2022-12-01 21:28:47 -0300

    pegando videos da api

--- index.js
+++ index.js
@@ -10,9 +10,39 @@ async function PegandoInformacao() {
   const url = `https://api.nasa.gov/planetary/apod?api_key=${apikey}&date=${input}`;
   const resposta = await fetch(url);
   const data = await resposta.json();
-  document.getElementById("data").innerHTML = data.date;
+  console.log(data);
   document.querySelector("#legenda").innerText = data.explanation;
-  document.getElementsByTagName(
-    "body"
-  )[0].style = `background-image:url(${data.url});`;
+  var dataInput = data.date;
+  console.log(dataInput + "data");
+  dados = new Date(dataInput);
+  dataFormatada = dados.toLocaleDateString("pt-BR", { timeZone: "UTC" });
+  document.getElementById("data").innerHTML = dataFormatada;
+  console.log(dataFormatada);
+
+  if (data.media_type === "image") {
+    document.getElementsByTagName(
+      "body"
+    )[0].style = `background-image:url(${data.url});`;
+  }
+
+  if (data.media_type === "video") {
+    let video = document.querySelector("#img");
+    video.innerHTML = `<iframe width="853" height="480" src="${data.url}"></iframe>`;
+    document.getElementsByTagName(
+      "body"
+    )[0].style = `background-image:url(cat.gif);`;
+  }
+
+  if (data.media_type === "image") {
+    let vazio = document.querySelector("#img");
+    vazio.innerHTML = ` `;
+  }
+
+  if (data.date === undefined) {
+    document.querySelector("#legenda").innerText =
+      "você ainda não tem uma maquina do tempo pra viajar para o futuro";
+    document.getElementsByTagName(
+      "body"
+    )[0].style = `background-image:url(caveira.gif);`;
+  }
 }
